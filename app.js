9a40caa162e96e55402ccc4ffb4a5f120e1df7d5
Gopesh4 <165290282+Gopesh4@users.noreply.github.com>
2024-05-15 00:45:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 //JS will be writtem here.
 const API_KEY = '138e5e9c6c410d915e605bb3b98f197e';
-
-
 function showdeg(){
     var CITY = document.getElementById("input").value.toUpperCase();
     console.log(CITY)
     var WEATHER_API = `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=metric`;
 const cityname = document.getElementById("city");
 cityname.innerHTML = CITY;
-
 const temperature = document.querySelector('#temperature');
 const weatherIcon = document.querySelector('.temp-box img');
 const description = document.querySelector('#clouds');
@@ -23,3 +20,5 @@ const description = document.querySelector('#clouds');
   })
   .catch(error => console.log(error));
 }
+
+

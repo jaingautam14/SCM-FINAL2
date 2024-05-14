fddb0df38cdcec17ae67380fdbb78e070a4deec8
jaingautam14 <gautam0336.be23@chitkara.edu.in>
2024-05-14 18:26:17 +0530

    Link for the logo and data added

--- app.js
+++ app.js
@@ -8,3 +8,18 @@ function showdeg(){
     var WEATHER_API = `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=metric`;
 const cityname = document.getElementById("city");
 cityname.innerHTML = CITY;
+
+const temperature = document.querySelector('#temperature');
+const weatherIcon = document.querySelector('.temp-box img');
+const description = document.querySelector('#clouds');
+    fetch(WEATHER_API)
+    .then(response => response.json())
+   .then(data => {
+    console.log(data);
+    const iconURL = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    weatherIcon.setAttribute('src', iconURL);
+    temperature.textContent = `${data.main.temp}°C`;
+    description.textContent = data.weather[0].description;
+  })
+  .catch(error => console.log(error));
+}

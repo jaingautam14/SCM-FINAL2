3a0bfcac5621c170bc2c3e452897b8da7cbcc423
jaingautam14 <gautam0336.be23@chitkara.edu.in>
2024-05-14 18:25:30 +0530

    Link for the api added

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 //JS will be writtem here.
 const API_KEY = '138e5e9c6c410d915e605bb3b98f197e';
+
+
+function showdeg(){
+    var CITY = document.getElementById("input").value.toUpperCase();
+    console.log(CITY)
+    var WEATHER_API = `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=metric`;
+const cityname = document.getElementById("city");
+cityname.innerHTML = CITY;
